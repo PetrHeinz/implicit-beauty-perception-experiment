1b09229f9d85ddf24f2b69a5d17c8257c345956c
Petr Heinz <petr.heinz@niceincontact.com>
2021-11-12 19:26:36 +0100

    Basic template of the Start screen

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# Implicit Beauty Perception Experiment
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="Start">
+            <div className="Start-logo">✵</div>
+            <h1 className="Start-text">Implicit Beauty Perception Experiment</h1>
+            <button className="Start-button" onClick={event => event.target.textContent = 'No.'}>Start</button>
+        </div>
+    );
 }
-
-export default App;
