cc4efc3d76b1b6c32497aff10f9c06f79fba5c73
Petr Heinz <petr.heinz@niceincontact.com>
2021-11-15 18:21:13 +0100

    Log started experiment

--- src/Start.js
+++ src/Start.js
@@ -2,11 +2,16 @@ import './Start.css';
 import { PAGE_SELECT } from "./App";
 
 export default function Start(props) {
+    function onStart() {
+        console.log("Started");
+        props.changePage(PAGE_SELECT);
+    }
+
     return (
         <div className="Start">
             <div className="Start-logo">✵</div>
             <h1 className="Start-text">Implicit Beauty Perception Experiment</h1>
-            <button className="Start-button" onClick={() => props.changePage(PAGE_SELECT)}>Start</button>
+            <button className="Start-button" onClick={onStart}>Start</button>
         </div>
     );
 }
