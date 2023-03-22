d29164b8a8c3def2a098c10f911c46c1e7273180
Petr Heinz <petr@betterstack.com>
2023-03-22 19:07:44 +0100

    move choice buttons more to the left

--- src/ChoiceButton.js
+++ src/ChoiceButton.js
@@ -2,7 +2,7 @@ export default function ChoiceButton(props) {
     return (
         <button className="Choice-button"
                 onClick={props.onClick}
-                style={{left: 5 + (30 * props.x) + "vw", top: 5 + (30 * props.y) + "vh"}}
+                style={{left: 5 + (15 * props.y) + "vw", top: 5 + (30 * props.y) + "vh"}}
         >
             Vybrat
         </button>
